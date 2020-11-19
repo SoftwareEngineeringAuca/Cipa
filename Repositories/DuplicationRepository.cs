1af4998eb142fa5a1ce15080dc5b1c203bb38f2f
Filipp <egor.olenburg@timelysoft.net>
2020-11-19 18:24:03 +0600

    repo for duplication workflow

---
+++ Repositories/DuplicationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cipa.Helpers;
+using Cipa.Interfaces;
+
+namespace Cipa.Repositories
+{
+    public class DuplicationRepository: Base, IDuplicationRepository
+    {
+        public ExecuteResult ExecuteWorkCodeQuery(string userCode)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
